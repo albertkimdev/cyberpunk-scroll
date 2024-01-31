02fb95abd9cfc2cb5fa736b26350b837612d9d3e
albertkim91 <albertkim91@gmail.com>
2024-01-31 16:14:17 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,117 +1,145 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useEffect, useRef, useState } from "react";
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  const images = [
+    "/img1.jpg",
+    "/img2.jpg",
+    "/img3.jpg",
+    "/img4.jpg",
+    "/img5.jpg",
+    "/img6.jpg",
+    "/img7.jpg",
+    "/img1.jpg",
+    "/img2.jpg",
+    "/img3.jpg",
+    "/img4.jpg",
+    "/img5.jpg",
+    "/img6.jpg",
+    "/img7.jpg",
+  ];
+
+  const [target, setTarget] = useState(0);
+  const [current, setCurrent] = useState(0);
+  const [ease, setEase] = useState(0.075);
+  const [maxScroll, setMaxScroll] = useState(0);
+
+  const slider = useRef();
+  const sliderWrapper = useRef();
+  const slides = useRef([]);
+  const requestRef = useRef();
+
+  const lerp = (start, end, factor) => start + (end - start) * factor;
+
+  const updateScaleAndPosition = () => {
+    if (!slides.current || typeof window === "undefined") return;
+    slides.current.forEach((slide) => {
+      const rect = slide.getBoundingClientRect();
+      const centerPosition = (rect.left + rect.right) / 2;
+      const distanceFromCenter = centerPosition - window.innerWidth / 2;
+
+      let scale, offsetX;
+
+      if (distanceFromCenter > 0) {
+        scale = Math.min(1.75, 1 + distanceFromCenter / window.innerWidth);
+        offsetX = (scale - 1) * 300;
+      } else {
+        scale = Math.max(
+          0.5,
+          1 - Math.abs(distanceFromCenter) / window.innerWidth
+        );
+        offsetX = 0;
+      }
+
+      gsap.set(slide, { scale, x: offsetX });
+    });
+  };
+
+  const update = () => {
+    const newCurrent = lerp(current, target, ease);
+    setCurrent(newCurrent);
+
+    gsap.set(sliderWrapper.current, {
+      x: -newCurrent,
+    });
+
+    updateScaleAndPosition();
+    requestRef.current = requestAnimationFrame(update);
+  };
+
+  useEffect(() => {
+    if (sliderWrapper.current) {
+      setMaxScroll(sliderWrapper.current.offsetWidth - window.innerWidth);
+    }
+  }, [sliderWrapper]);
+
+  useEffect(() => {
+    const handleWheel = (e) => {
+      let newTarget = target + e.deltaY;
+      newTarget = Math.max(0, newTarget);
+      newTarget = Math.min(maxScroll, newTarget);
+      setTarget(newTarget);
+    };
+    window.addEventListener("wheel", handleWheel);
+
+    window.addEventListener("resize", () => {
+      setMaxScroll(sliderWrapper.current.offsetWidth - window.innerWidth);
+    });
+
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, [target, maxScroll]);
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [current, target]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="w-full h-full techno-charm">
+      {/* sidebar */}
+      <div
+        className="flex w-[100vh] py-[1.5em] px-[3em]"
+        style={{
+          transform: "rotate(-90deg) translate(-100%, 0)",
+          transformOrigin: "left top",
+        }}
+      >
+        {/* sidebar item */}
+        <div className="flex-[2]">
+          <p className="uppercase text-[7.5rem] leading-[85%] mb-[4rem] font-techno-charm">
+            Cyber
+            <br />
+            Punk
+          </p>
+          <p>Explore the computer</p>
+        </div>
+
+        {/* sidebar item */}
+        <div className="flex-[2] flex gap-[6em]">
+          <p className="font-thin">Scolling experience</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* slider */}
+      <div ref={slider} className="w-full h-full overflow-hidden">
+        <div
+          ref={sliderWrapper}
+          className="absolute top-0 w-[8000px] px-[600px] py-0 h-full flex items-center gap-[100px]"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {images.map((img, i) => (
+            <div
+              ref={(el) => (slides.current[i] = el)}
+              className="w-[400px] h-[500px] bg-[#e3e3e3]"
+              key={i}
+            >
+              <img className="w-full h-full object-cover" src={img} />
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
